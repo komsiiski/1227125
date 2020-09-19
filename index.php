8822c83428bb3eaac3c3d3a8f384297379493066
komsiiski <kalata2407@abv.bg>
2020-09-19 09:45:00 +0300

    updating index.php

--- index.php
+++ index.php
@@ -1,2 +1,23 @@
 <?php
 echo "Hello world";
+$username = "komsiiski";
+echo $username;
+echo "My name is $username <br>";
+$age = 22;
+echo $age;
+$istrue = true;
+echo $istrue;
+$usernameArray = ["kaloyan", "komsiiski"];
+echo $usernameArray[0];
+echo $usernameArray[1];
+$usernameAsocArray = ["username"=>"kaloqn", "age"=>22];
+echo $usernameAsocArray["username"];
+echo $usernameAsocArray["age"];
+for($i=0; $i<2; $i++)
+{
+    echo $usernameArray[$i];
+}
+foreach($usernameAsocArray as $key=>$item)
+{
+    echo $key." ".$item."<br>";
+}
